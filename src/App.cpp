e5cb1277a714ee545b2e113766da715393a4e13d
JoshuaCCrass <JCCrass3139@gmail.com>
2022-03-09 12:15:48 -0600

    New shader

--- src/App.cpp
+++ src/App.cpp
@@ -1,26 +1,29 @@
 #include "App.hpp"
 
-App::App(){
+App::App()
+{
   Engine::Log("Object Made");
 }
-App::~App(){
+App::~App()
+{
   Engine::Log("Object Destroyed");
 }
 
-void App::Run(){
+void App::Run()
+{
   if (appState == AppState::ON)
     Engine::FatalError("App already running.");
-  
+
   Engine::Init();
 
   unsigned int windowFlags = 0;
 
-  //windowFlags |= Engine::WindowFlags::FULLSCREEN;
+  // windowFlags |= Engine::WindowFlags::FULLSCREEN;
 
-  //windowFlags |= Engine::WindowFlags::BORDERLESS;
+  // windowFlags |= Engine::WindowFlags::BORDERLESS;
 
   window.Create("Engine", 800, 600, windowFlags);
-  
+
   Load();
 
   appState = AppState::ON;
@@ -31,49 +34,72 @@ void App::Run(){
 void App::Load()
 {
   // build and compile our shader program
-  shader.Compile("assets/shaders/3.1.shader.vs", "assets/shaders/3.1.shader.fs");
+  // ------------------------------------
+  shader.Compile("assets/shaders/4.2.texture.vs","assets/shaders/4.2.texture.fs");
+  shader.AddAttribute("ourColor");
+  shader.AddAttribute("aTexCoord");
   shader.Link();
+  
 
-  float vertices[] = 
-  {
-    -0.5f, -0.5, 0.0f, //left
-    0.5f, -0.5, 0.0f, //right
-    0.0f, 0.5, 0.0f, //top
-    -0.25f, 0.0, 0.0f, //left mid
-    0.0f, -0.5f, 0.0f, //bottom center
-    0.25f, 0.0f, 0.0f //right mid
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  float vertices[] = {
+      // positions          // colors           // texture coords
+       0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,   // top right
+       0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f,   // bottom right
+      -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+      -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f    // top left 
   };
 
   unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3,
+      0, 1, 3, // first triangle
+      1, 2, 3, // second triangle
   };
 
-  // VBO , VAO
+  // unsigned int VBO, VAO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
-  //
+  glGenBuffers(1, &EBO);
+  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+  // position
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // color
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // texture coord
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // load texture 1
+  texture1 = Engine::LoadPNGToGLTexture("assets/textures/container.png", GL_RGBA, GL_RGBA);
+  // load texture 2
+  texture2 = Engine::LoadPNGToGLTexture("assets/textures/awesomeface.png", GL_RGBA, GL_RGBA);
+
+  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  glBindVertexArray(0); // unbind our vertex
+  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+  // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+  glBindVertexArray(0);
 
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glPolygonMode( GL_FRONT_AND_BACK, GL_FILL);
 }
+
 void App::Loop()
 {
-  while(appState == AppState::ON)
+  while (appState == AppState::ON)
   {
     Update();
     Draw();
-    
     // Get SDL to swap our buffer
     window.SwapBuffer();
     LateUpdate();
@@ -81,32 +107,33 @@ void App::Loop()
     InputUpdate();
   }
 }
-void App::Update(){}
+void App::Update() {}
 void App::Draw()
 {
-      glClearColor(0.2f,0.3f,0.3f,1.0f);
-      glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT);
 
-      // be sure to activate the shader program before calling to uniforms
-      shader.Use();
+  // bind textures on corresponding texture units
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture1.id);
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, texture2.id);
 
-      // update shader uniform
-      double timeValue = SDL_GetTicks() / 1000;
-      float greenValue = static_cast<float>(sin(timeValue) / 2.0 + 0.5);
-      //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-      //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+  // be sure to activate the shader before any calls to glUniform
+  shader.Use();
 
-      shader.SetVec4("ourColor", glm::vec4(0.0f, greenValue, 0.0f, 1.0f));
+  glUniform1i(glGetUniformLocation(shader.GetProgramID(), "texture1"), 0);
+  glUniform1i(glGetUniformLocation(shader.GetProgramID(), "texture2"), 1);
 
-      // render triangle
-      glBindVertexArray(VAO);
-      glDrawArrays(GL_TRIANGLES, 0, 6);
-      glBindVertexArray(0);
+  // render the triangle
+  glBindVertexArray(VAO);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  glBindVertexArray(0);
 
-      shader.UnUse();
-    }
-void App::LateUpdate(){}
-void App::FixedUpdate(float _delta_time){}
+  shader.UnUse();
+}
+
+void App::LateUpdate() {}
+void App::FixedUpdate(float _delta_time) {}
 void App::InputUpdate()
 {
   SDL_Event event;
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-
+    unsigned int myBits = 0x00000010;
+    std::cout << myBits << std::endl;
     App app;
 
     app.Run();
