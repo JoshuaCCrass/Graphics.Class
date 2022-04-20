9476c686cdc810bb53ae32cdbdbdf1036e73ca78
Joshua Crass <JCCrass3139@gmail.com>
2022-04-20 13:03:31 -0500

    Backpack and text

--- src/App.cpp
+++ src/App.cpp
@@ -268,13 +268,28 @@ void App::Draw()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     // don't forget to enable shader before setting uniforms
-    
+    modelShader.Use();
 
     // view/projection transformations
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)window.GetScreenWidth() / (float)window.GetScreenHeight(), 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    modelShader.SetMat4("projection", projection);
+    modelShader.SetMat4("view", view);
 
     // render the loaded model
+    glm::mat4 modelTransfrom = glm::mat4(1.0f);
+    modelTransfrom = glm::translate(modelTransfrom, glm::vec3(1.0f));
+    modelTransfrom = glm::scale(modelTransfrom, glm::vec3(1.0f));
+    modelShader.SetMat4("model", modelTransfrom);
+    model.Draw(modelShader);
+    modelShader.UnUse();
     
     // render text
+    textShader.Use();
+    projection = glm::ortho(0.0f, static_cast<float>(window.GetScreenWidth()), 0.0f, static_cast<float>(window.GetScreenHeight()));
+    textShader.SetMat4("projection", projection);
+    RenderText(textShader, "Computer Graphics 12:10", 25.0f, 25.0f, 1.0f, glm::vec3(0.5f, 0.8f, 0.2f));
+    textShader.UnUse();
 }
 
 void App::LateUpdate() {}
